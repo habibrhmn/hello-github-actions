b8b7602147eca5b9e554fc5ba513439fdb886314
HabibRahman <45668654+kappy123@users.noreply.github.com>
2022-05-04 11:32:42 +0530

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -4,4 +4,4 @@ sh -c "echo Hello world my name is $INPUT_WORLD"
 
 sh -c "echo This is a $INPUT_NAME here"
 
-sh -c "apt install apache2 && systemctl restart apache2 && ip r"
+sh -c "systemctl restart apache2 && ip r"
